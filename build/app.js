b9315801c15510d0e42eb543143d13dafe6ff929
Darylabrador <abrador.daryl@gmail.com>
2021-07-19 12:09:29 +0400

    Simple routing withtout router.ts

--- build/app.js
+++ build/app.js
@@ -3,13 +3,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var http_1 = require("http");
 var port = 3000;
 var server = http_1.createServer(function (request, response) {
-    var url = request.url;
-    if (url === "/") {
-        response.write('<html>');
-        response.write('<head> <title> Entrer message</title></head>');
-        response.write('<body><p> Node JS + TypeScript </p></body>');
-        response.write('</html>');
-        return response.end();
+    switch (request.url) {
+        case '/': {
+            if (request.method === 'GET') {
+                response.write('<html>');
+                response.write('<head> <title> TypeScript CMS </title></head>');
+                response.write('<body><p> Node JS + TypeScript </p></body>');
+                response.write('</html>');
+                return response.end();
+            }
+            break;
+        }
+        default: {
+            response.statusCode = 404;
+            response.end();
+        }
     }
 });
 server.listen(port);
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,24 @@
 import { createServer, IncomingMessage, ServerResponse } from 'http';
- 
+
 const port = 3000;
- 
+
 const server = createServer((request: IncomingMessage, response: ServerResponse) => {
-    const url    = request.url;
-    if (url === "/") {
-        response.write('<html>');
-        response.write('<head> <title> Entrer message</title></head>');
-        response.write('<body><p> Node JS + TypeScript </p></body>');
-        response.write('</html>');
-        return response.end();
+    switch (request.url) {
+        case '/': {
+            if (request.method === 'GET') {
+                response.write('<html>');
+                response.write('<head> <title> TypeScript CMS </title></head>');
+                response.write('<body><p> Node JS + TypeScript </p></body>');
+                response.write('</html>');
+                return response.end();
+            }
+            break;
+        }
+        default: {
+            response.statusCode = 404;
+            response.end();
+        }
     }
 });
- 
+
 server.listen(port);
